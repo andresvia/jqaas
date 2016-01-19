04e6b59f8d9cfa4c01c41dea65b290950dc95881
Andres Villarroel <andres.via@gmail.com>
2016-01-19 05:32:06 -0800

    Update app.js

--- app.js
+++ app.js
@@ -20,28 +20,10 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.send(err.message);
 });
 
 module.exports = app;
